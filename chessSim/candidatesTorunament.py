a512683f22bb5a363a7a4248be07eac5184aeb9b
Caleb Wetherell <cmwetherell@gmail.com>
2022-06-21 11:21:41 -0700

    cand rd 4

--- chessSim/candidatesTorunament.py
+++ chessSim/candidatesTorunament.py
@@ -35,6 +35,11 @@ def playChess(model, whitePlayer, blackPlayer, format):
 
     return result
 
+def second_largest(list_input):
+    # print(list_input)
+    # print(type(list_input))
+    return list(list_input.sort_values(ascending=False))[-2]
+
 class Candidates:
     
     def __init__(self, players, games = None):
@@ -55,12 +60,13 @@ class Candidates:
         self.games['stage'] = 'rr'
         self.games['played'] = 0
         self.games['result'] = 0
+        
 
         # self.games.to_csv("./chessSim/data/candidatesGames.csv", index=False)
 
     def simRR(self):
         #https://handbook.fide.com/files/handbook/Regulations_for_the_FIDE_Candidates_Tournament_2022.pdf
-
+        self.games['blackWin'] = 0
 
         for idx, row in self.games.iterrows():
             if row.played == 0:
@@ -72,27 +78,46 @@ class Candidates:
 
                 #TODO should I use the update ratings function or not?
                 self.games.at[idx, 'result'] = result
+        # print(self.games)
         
-        whiteResults = self.games[['whitePlayer', 'result']].values
-        blackResults = self.games[['blackPlayer', 'result']].values
-        blackResults[:, 1] = 1 - blackResults[:, 1] # to change the game results to black player POV
+        whiteResults = self.games[['whitePlayer', 'blackPlayer', 'result', 'blackWin']].values
+        blackResults = self.games[['blackPlayer', 'whitePlayer', 'result', 'blackWin']].values
+        blackResults[:, 2] = 1 - blackResults[:, 2] # to change the game results to black player POV
+        blackResults[:, 3] = 1 * (blackResults[:, 2] == 1) #checking for black wins, used for tb
         
-        self.rrSummary = pd.DataFrame(np.concatenate(
+        self.tbrrSummary = pd.DataFrame(np.concatenate(
             (whiteResults, blackResults)
             , axis = 0))
-        self.rrSummary.columns = ['name', 'result']
-        self.rrSummary['wins'] = 1 * (self.rrSummary.result == 1)
-        self.rrSummary = self.rrSummary.groupby(['name']).agg(
-             score = ('result','sum'),
-             wins = ('wins','sum'),
-             ).sort_values(by = ['score', 'wins'], ascending = False).reset_index()
-        self.rrSummary['first'] = 1 * (self.rrSummary.score == max(self.rrSummary.score))
+        self.tbrrSummary.columns = ['name', 'oppName','result', 'blackWin']
+        self.tbrrSummary['wins'] = 1 * (self.tbrrSummary.result == 1)
 
-        self.remaining = list(self.rrSummary[self.rrSummary['first'] == 1].name)
+        tmpScores = self.tbrrSummary.groupby(['name']).agg(
+            score = ('result','sum')).reset_index() # df[name, score]
+
+        #add SB tiebreak, move sort values code to this df
+        self.tbrrSummary['sbPoints'] = 0
+        for idx, row in self.tbrrSummary.iterrows():
+            self.tbrrSummary.at[idx, 'sbPoints'] = row.result * tmpScores.loc[tmpScores.name == row.oppName, 'score'].values[0]
+
+
+        self.tbrrSummary = self.tbrrSummary.groupby(['name']).agg( #using more sorting mechanisms than needed, but still advancing everyone with same score to TBs
+            score = ('result','sum'),
+            sb = ('sbPoints', 'sum'),
+            wins = ('wins','sum'),
+            blackWins = ('blackWin','sum'),
+            ).sort_values(by = ['score', 'sb', 'wins', 'blackWins'], ascending = False).reset_index()
+
+
+        self.tbrrSummary['first'] = 1 * (self.tbrrSummary.score == max(self.tbrrSummary.score))
+
+
+
+        self.remaining = list(self.tbrrSummary[self.tbrrSummary['first'] == 1].name)
         if len(self.remaining) == 1:
             self.winner = self.remaining[0]
 
-        # print(self.games)
+        self.second = list(self.tbrrSummary.name)[1] #Gets second entry of sorted names based on FIDE regulations TB criteria, overwritten later if there is TBs
+
 
     def tieS12(self):
 
@@ -110,26 +135,28 @@ class Candidates:
 
                 if stage == 's1':
                     game1 = playChess(bst, player1, player2, format = 'r') #points white player scored
-                    self.games.loc[self.games.shape[0]] = [player1.name, player2.name, 'r', 's1', 1, game1]
+                    self.games.loc[self.games.shape[0]] = [player1.name, player2.name, 'r', 's1', 1, game1, 1*(game1==0)]
 
                     game2 = playChess(bst, player2, player1, format = 'r') #points white player scored
-                    self.games.loc[self.games.shape[0]] = [player2.name, player1.name, 'r', 's1', 1, game2]
+                    self.games.loc[self.games.shape[0]] = [player2.name, player1.name, 'r', 's1', 1, game2, 1*(game1==0)]
 
                 elif stage == 's2':
                     game1 = playChess(bst, player1, player2, format = 'b') #points white player scored
-                    self.games.loc[self.games.shape[0]] = [player1.name, player2.name, 'b', 's2', 1, game1]
+                    self.games.loc[self.games.shape[0]] = [player1.name, player2.name, 'b', 's2', 1, game1, 1*(game1==0)]
 
                     game2 = playChess(bst, player2, player1, format = 'b') #points white player scored
-                    self.games.loc[self.games.shape[0]] = [player2.name, player1.name, 'b', 's2', 1, game2]
+                    self.games.loc[self.games.shape[0]] = [player2.name, player1.name, 'b', 's2', 1, game2, 1*(game1==0)]
 
                 player1Score = game1 + (1 - game2)
 
                 if player1Score > 1:
                     self.winner = player1.name
+                    self.second = player2.name
                 elif player1Score == 1:
                     pass #move to s2 because remaining players will be > 1
                 elif player1Score < 1:
                     self.winner = player2.name
+                    self.second = player1.name
 
                 # print(s1Players)
             if len(s1Players) > 2:
@@ -193,6 +220,10 @@ class Candidates:
                 self.remaining = list(self.rrSummary[self.rrSummary['first'] == 1].name)
                 if len(self.remaining) == 1:
                     self.winner = self.remaining[0]
+                    self.rrSummary['second'] = 1 * (self.rrSummary.score == second_largest(self.rrSummary.score))
+                    self.secondList = list(self.rrSummary[self.rrSummary['second'] == 1].name)
+                    if len(self.secondList) == 1:
+                        self.second = self.secondList[0]
 
 
     def tieS3(self): #janky knockout tournament code that seems to work pretty well!
@@ -223,12 +254,13 @@ class Candidates:
                     elif result < 1: #technically, if it's a draw the game should be replayed and colors swapped, but this is good enough
                         loser = whitePlayer.name
                     losers.append(loser)
-
+                    self.second = loser # last loser is second place
                     games.at[idx, 'result'] = result
                 
                 self.games = pd.concat([self.games, games], ignore_index = True, axis = 0)
 
             for loser in losers:
+
                 self.remaining.remove(loser)
             
         if len(self.remaining) == 1:
@@ -252,6 +284,10 @@ class Candidates:
             # print(self.rrSummary)
         if not hasattr(self, 'winner'):
             print('something is broken, there is no winner after stage 3 is over')
+        if not hasattr(self, 'second'):
+            print('something is broken, there is no second after stage 3 is over')
+            print(self.games)
+            print(self.tbrrSummary)
 
 
 # tournament = Candidates(getCandidates())
