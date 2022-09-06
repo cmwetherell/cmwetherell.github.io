ce981dcba893fc4652c60df339ef1d08afbc02c3
Caleb Wetherell <cmwetherell@gmail.com>
2022-09-06 16:10:51 -0700

    rd 5

--- chessSim/runSims.py
+++ chessSim/runSims.py
@@ -24,7 +24,7 @@ def main():
 
 # ##python poolOdds.py 100 True <- terminal command to get results for 100 sims with new pool draws for GP2
 
-    nSims = 5000
+    nSims = 10000
     if len(terminalArgs) > 1:
         nSims = int(terminalArgs[1])
     
